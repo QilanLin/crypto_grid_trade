412e8851111d6839ffd76d55253d182168e79120
Qilan Lin <78576883+QilanLin@users.noreply.github.com>
2025-01-31 03:13:39 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+SMTP_SERVER = 'smtp.example.com'
+SMTP_PORT = 465
+SMTP_USER = 'your_email@example.com'
+SMTP_PASSWORD = 'your_email_password'
+RECIPIENT_EMAIL = 'recipient@example.com'
+
+SYMBOL = 'MELANIA/USDT'
+PRICE_RANGES = [
+    (0.9, 1.1),
+    (2.9, 3.1),
+    (3.9, 4.1)
+]
